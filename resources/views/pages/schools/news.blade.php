926391cae756d9ac9392a96ff38c5ac22ab92861
mondeer <keringlaban@yahoo.com>
2017-01-16 12:35:39 +0300

    news blade

---
+++ resources/views/pages/schools/news.blade.php
@@ -0,0 +1,51 @@
+@extends('pages.schools.schoolhome')
+
+@section('content')
+    <h3><i class="fa fa-angle-right"></i> Create A New Student</h3>
+     <!-- INLINE FORM ELELEMNTS -->
+    <div class="row mt">
+
+        <div class="col-lg-6">
+            <div class="panel panel-primary">
+                <div class="panel-heading">Fill in Student Details</div>
+                <div class="panel-body">
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            {{$errors->first()}}
+                        </div>
+                    @endif
+
+                    <form class="form-horizontal" role="form" method="POST" enctype="multipart/form-data"
+                          action="{{ url('/pages/news/create') }}">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Title</label>
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="title" value="{{ old('title') }}">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Content</label>
+                            <div class="col-md-6">
+                                <input type="textarea" class="form-control" name="content" value="{{ old('content') }}">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">Submit News</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div><!-- /col-lg-12 -->
+
+        <div class="col-lg-6">
+
+        </div>
+
+    </div><!-- /row -->
+@endsection
